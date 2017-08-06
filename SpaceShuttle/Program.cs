7e3679e0f492fc4ce9cb06911aa0cd07d5cd93c0
eviansoft <30775843+eviansoft@users.noreply.github.com>
2017-08-06 19:42:40 +0300

    Hi

--- SpaceShuttle/Program.cs
+++ SpaceShuttle/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SpaceShuttle
 {
@@ -10,6 +6,7 @@ namespace SpaceShuttle
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hi!");
         }
     }
 }
