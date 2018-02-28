ee0271d8033b611912086f5a7382bcac0e5bb330
eviansoft <30775843+eviansoft@users.noreply.github.com>
2018-02-28 15:31:48 +0200

    Test rename 3

--- SpaceShuttle/Program.cs
+++ SpaceShuttle/Program.cs
@@ -9,6 +9,7 @@ namespace SpaceShuttle
             Console.WriteLine("Hi+Hi!");
             Console.WriteLine("From renamed branch");
             Console.WriteLine("From renamed branch 2!");
+            Console.WriteLine("From renamed branch 3!!");
         }
     }
 }
