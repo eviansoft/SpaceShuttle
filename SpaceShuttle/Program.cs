664be3eddefe9591b8a3498394cb0a01f008d8bb
eviansoft <30775843+eviansoft@users.noreply.github.com>
2018-07-29 22:58:41 +0300

    flying action

--- SpaceShuttle/Program.cs
+++ SpaceShuttle/Program.cs
@@ -1,13 +1,35 @@
 ﻿using System;
+using System.Timers;
 
 namespace SpaceShuttle
 {
     class Program
     {
+        public static int i = 10;
+        public static Timer timer = new Timer();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi+Hi!");
-            Console.WriteLine("From renamed branch 4!!!");
+            timer.Interval = 1000;
+            timer.Elapsed += Timer_Elapsed;
+
+            Console.WriteLine("Запускаем");
+            timer.Start();
+
+            Console.ReadLine();
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(i);
+            if(i == 0)
+            {
+                timer.Stop();
+                Console.WriteLine("ПОЛЕТЕЛИИИИИИИИИИИИИИИИИИИ!!!!!!!!!!!!!!");
+                Console.Beep(500, 10000);
+            }
+
+            i--;
         }
     }
 }
